dc529d05c4d64a2cb4e1cec5f0f817d310d6df80
tomxi1997 <78306449+tomxi1997@users.noreply.github.com>
2023-08-23 21:18:54 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # global properties
 properties() { '
 kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
@@ -29,7 +29,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
